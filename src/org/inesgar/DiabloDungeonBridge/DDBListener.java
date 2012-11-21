1629d2581043259110494e3afc73e45383344df8
Richard Harrah <topplethenun@hawksharbor.info>
2012-11-21 15:30:58 -0500

    f - chest generation

--- src/org/inesgar/DiabloDungeonBridge/DDBListener.java
+++ src/org/inesgar/DiabloDungeonBridge/DDBListener.java
@@ -1,13 +1,13 @@
 package org.inesgar.DiabloDungeonBridge;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
+import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
 
 import com.modcrafting.diablodrops.events.RuinGenerateEvent;
 import com.timvisee.DungeonMaze.event.generation.DMGenerationChestEvent;
@@ -32,17 +32,16 @@ public class DDBListener implements Listener
 		{
 			event.getContents().clear();
 		}
-		List<CraftItemStack> itemstacks = new ArrayList<CraftItemStack>();
-		int size = rand.nextInt(((Chest) event.getBlock().getState())
-				.getInventory().getSize());
-		for (int i = 0; i < size; i++)
+		Block block = event.getBlock();
+		Chest chestB = ((Chest) block.getState());
+		Inventory chest = chestB.getBlockInventory();
+		for (int i = 0; i < rand.nextInt(chest.getSize()); i++)
 		{
-			CraftItemStack cis = null;
+			CraftItemStack cis = plugin.dd.dropsAPI.getItem();
 			while (cis == null)
 				cis = plugin.dd.dropsAPI.getItem();
-			itemstacks.add(cis);
+			chest.setItem(i, cis);
 		}
-		event.getContents().addAll(itemstacks);
 	}
 
 	@EventHandler
