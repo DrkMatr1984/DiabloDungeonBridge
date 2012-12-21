2bb728278714c016ed589b6062323aca661a84da
Richard Harrah <topplethenun@hawksharbor.info>
2012-12-20 21:26:32 -0500

    ud - Bukkit

--- src/org/inesgar/DiabloDungeonBridge/DDBListener.java
+++ src/org/inesgar/DiabloDungeonBridge/DDBListener.java
@@ -9,17 +9,16 @@ import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
-import org.bukkit.craftbukkit.entity.CraftMonster;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.inventory.ItemStack;
 
 import com.modcrafting.diablodrops.events.EntitySpawnWithItemEvent;
 import com.modcrafting.diablodrops.events.RuinGenerateEvent;
-import com.modcrafting.diablolibrary.entities.DiabloMonster;
-import com.modcrafting.diablolibrary.items.DiabloItemStack;
 import com.timvisee.DungeonMaze.API.DungeonMazeAPI;
 import com.timvisee.DungeonMaze.event.generation.DMGenerationChestEvent;
 
@@ -62,41 +61,65 @@ public class DDBListener implements Listener
     @EventHandler
     public void onCreatureSpawn(final CreatureSpawnEvent event)
     {
+        LivingEntity entity = event.getEntity();
         int chance = plugin.dd.config.getInt("Precentages.ChancePerSpawn", 9)
                 * DungeonMazeAPI.getDMLevel(event.getLocation().getBlock());
         if (chance >= (plugin.dd.gen.nextInt(100) + 1))
         {
-            CraftMonster cm;
-            try
+            List<ItemStack> items = new ArrayList<ItemStack>();
+            for (int i = 0; i < (plugin.dd.gen.nextInt(5) + 1); i++)
             {
-                cm = (CraftMonster) event.getEntity();
-            }
-            catch (Exception e)
-            {
-                return;
-            }
-            DiabloMonster dle = new DiabloMonster(cm);
-            List<DiabloItemStack> items = new ArrayList<DiabloItemStack>();
-            DiabloItemStack ci = plugin.dd.dropsAPI.getItem();
-            while (ci == null)
-            {
-                ci = plugin.dd.dropsAPI.getItem();
-            }
-            if (plugin.config.getBoolean("Custom.Only", false))
-            {
-                ci = plugin.dd.custom.get(plugin.dd.gen
-                        .nextInt(plugin.dd.custom.size()));
-            }
-            if (ci != null)
-            {
-                items.add(ci);
+                ItemStack ci = plugin.dd.dropsAPI.getItem();
+                while (ci == null)
+                {
+                    ci = plugin.dd.dropsAPI.getItem();
+                }
+                if (plugin.config.getBoolean("Custom.Only", false)
+                        && plugin.config.getBoolean("Custom.Enabled", true))
+                {
+                    ci = plugin.dd.custom.get(plugin.dd.gen
+                            .nextInt(plugin.dd.custom.size()));
+                }
+                if (ci != null)
+                {
+                    items.add(ci);
+                }
             }
             EntitySpawnWithItemEvent eswi = new EntitySpawnWithItemEvent(
-                    event.getEntity(), items);
+                    entity, items);
             plugin.getServer().getPluginManager().callEvent(eswi);
             if (eswi.isCancelled())
                 return;
-            dle.setEquipment(items.toArray(new DiabloItemStack[0]));
+
+            for (ItemStack cis : eswi.getItems())
+                if (cis != null)
+                {
+                    if (plugin.dd.drop.isHelmet(cis.getType()))
+                    {
+                        entity.getEquipment().setHelmet(cis);
+                        entity.getEquipment().setHelmetDropChance(2.0F);
+                    }
+                    else if (plugin.dd.drop.isChestPlate(cis.getType()))
+                    {
+                        entity.getEquipment().setChestplate(cis);
+                        entity.getEquipment().setChestplateDropChance(2.0F);
+                    }
+                    else if (plugin.dd.drop.isLeggings(cis.getType()))
+                    {
+                        entity.getEquipment().setLeggings(cis);
+                        entity.getEquipment().setLeggingsDropChance(2.0F);
+                    }
+                    else if (plugin.dd.drop.isBoots(cis.getType()))
+                    {
+                        entity.getEquipment().setBoots(cis);
+                        entity.getEquipment().setLeggingsDropChance(2.0F);
+                    }
+                    else
+                    {
+                        entity.getEquipment().setItemInHand(cis);
+                        entity.getEquipment().setItemInHandDropChance(2.0F);
+                    }
+                }
         }
     }
 
--- src/org/inesgar/DiabloDungeonBridge/DDBTask.java
+++ src/org/inesgar/DiabloDungeonBridge/DDBTask.java
@@ -1,32 +1,33 @@
 package org.inesgar.DiabloDungeonBridge;
 
 import org.bukkit.block.Chest;
-import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class DDBTask implements Runnable
 {
 
-	private Chest chest;
+    private final Chest chest;
 
-	public DDBTask(Chest chest)
-	{
-		this.chest = chest;
-	}
+    public DDBTask(final Chest chest)
+    {
+        this.chest = chest;
+    }
 
-	@Override
-	public void run()
-	{
-		Inventory chestInv = chest.getBlockInventory();
-		for (int i = 0; i < DiabloDungeonBridge.instance.random
-				.nextInt(chestInv.getSize()); i++)
-		{
-			CraftItemStack cis = DiabloDungeonBridge.instance.dd.dropsAPI
-					.getItem();
-			while (cis == null)
-				cis = DiabloDungeonBridge.instance.dd.dropsAPI.getItem();
-			chestInv.setItem(i, cis);
-		}
-	}
+    @Override
+    public void run()
+    {
+        Inventory chestInv = chest.getBlockInventory();
+        for (int i = 0; i < DiabloDungeonBridge.instance.random
+                .nextInt(chestInv.getSize()); i++)
+        {
+            ItemStack cis = DiabloDungeonBridge.instance.dd.dropsAPI.getItem();
+            while (cis == null)
+            {
+                cis = DiabloDungeonBridge.instance.dd.dropsAPI.getItem();
+            }
+            chestInv.setItem(i, cis);
+        }
+    }
 
 }
